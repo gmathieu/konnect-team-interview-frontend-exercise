dc98eddc45b51169fcdbb8ffca03070d5fd57a8e
Guillaume Mathieu <contact@gmathieu.com>
2022-12-06 21:16:31 -0800

    fix: style layout

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
+import "@kong/kongponents/dist/style.css";
+
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
+import Kongponents from "@kong/kongponents";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(Kongponents);
 
 app.mount("#app");
