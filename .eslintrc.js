db2b5b66d4dbc121973d2dc5a925fa4bf6a66467
Guillaume Mathieu <contact@gmathieu.com>
2022-12-06 16:06:00 -0800

    build: add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/typescript/recommended',
+    'prettier',
   ],
   parserOptions: {
     ecmaVersion: 2020,
