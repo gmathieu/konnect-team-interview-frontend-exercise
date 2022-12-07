0070d0f9f6262d1262b173fcd19585c7a6a3b1fd
Guillaume Mathieu <contact@gmathieu.com>
2022-12-06 21:15:10 -0800

    build: add @kong/kongponents

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,15 @@ dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    commonjsOptions: {
+      include: [/@kong\/kongponents/, /node_modules/],
+    },
+  },
+  define: {
+    "process.env.development": JSON.stringify("development"),
+    "process.env.production": JSON.stringify("production"),
+  },
   plugins: [vue()],
   resolve: {
     alias: {
